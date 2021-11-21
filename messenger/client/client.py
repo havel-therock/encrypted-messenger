b6d6d873270ce3add72a42d828c9e74e98fc2683
MordechajWeiss <zaratusztra@protonmail.com>
2021-11-21 21:16:35 +0100

    added simple communication between clients via the server

--- messenger/client/client.py
+++ messenger/client/client.py
@@ -1,55 +1,115 @@
 import socket
 import hashlib
 import pickle
+import threading
+import time
+
 
 class Mess:
-  def __init__(self, msg_sender, msg_reciver, msg_hash, message):
-    self.msg_sender = msg_sender
-    self.msg_reciver = msg_reciver
-    self.msg_hash = msg_hash
-    self.message = message
+    def __init__(self, msg_sender, msg_receiver, msg_hash, message):
+        self.msg_sender = msg_sender
+        self.msg_receiver = msg_receiver
+        self.msg_hash = msg_hash
+        self.message = message
+
 
 HEADER_SIZE = 64
-PORT = 5051
-FORMAT ="utf-8"
+PORT = 5050
+FORMAT = "utf-8"
 DISCONNECTED_MESSAGE = "###DISCONNECTED###"
-SERVER="127.0.1.1"
+SERVER = "127.0.1.1"
 ADDR = (SERVER, PORT)
+
+server_response_flag = True
 
 
 def send_hello():
-    reciver = "server-hello"
-
-    m1 = Mess(nickname, reciver, "", "hello server my name is "+nickname)
+    receiver = "server-hello"
+    global server_response_flag
+    server_response_flag = False
+    m1 = Mess(nickname, receiver, "", "hello server my name is " + nickname)
     m1Pickle = pickle.dumps(m1)
     client.send(m1Pickle)
-    rec = client.recv(4096)  # magic number
-    msg_back = pickle.loads(rec)
-    print("Message-recived: {", msg_back.msg_sender, ",", msg_back.msg_reciver,
-          ",", msg_back.msg_hash, ",", msg_back.message, "}")
 
-def send(msg):
+    wait_iterator = 0
+    while not server_response_flag or wait_iterator > 10:
+        time.sleep(0.5)
+        wait_iterator += 1
+
+    if wait_iterator > 10:
+        print("server unreachable (hello)")
+
+
+    # rec = client.recv(4096)  # magic number
+    # msg_back = pickle.loads(rec)
+    # print("Message-received: {", msg_back.msg_sender, ",", msg_back.msg_receiver,
+    #      ",", msg_back.msg_hash, ",", msg_back.message, "}")
+
+
+def client_receiver():
+    global server_response_flag
+    while True:
+        rec = client.recv(4096)  # magic number
+        msg = pickle.loads(rec)
+        print("Message-received: {", msg.msg_sender, ",", msg.msg_receiver,
+              ",", msg.msg_hash, ",", msg.message, "}")
+        if msg.message == "OK" and ( msg.msg_sender == "server-authentication" or msg.msg_sender == "server-hello-authentication"):
+            server_response_flag = True
+        else:
+            localHash = hashlib.sha256(msg.message.encode()).hexdigest()
+            if msg.msg_hash == localHash:
+                m1 = Mess(nickname, "server-hash-return", localHash, "OK")
+                m1Pickle = pickle.dumps(m1)
+                client.send(m1Pickle)
+            else:
+                m1 = Mess(nickname, "server-hash-return", "", "NOK")
+                m1Pickle = pickle.dumps(m1)
+                client.send(m1Pickle)
+
+
+
+
+def simple_send_to_server(msg):
+    m1 = Mess(nickname, "server-hash-return", "", msg)
+    m1Pickle = pickle.dumps(m1)
+    client.send(m1Pickle)
+
+def send(receiver, msg):
     message_not_send = True
-    
-    while message_not_send:
-        reciver = "reciver112"
-        hashtmp = hashlib.sha256(msg.encode()).hexdigest()
-        m1 = Mess(nickname, reciver, hashtmp, msg)
-        m1Pickle = pickle.dumps(m1)
-        client.send(m1Pickle)
-        rec = client.recv(4096)     #magic number
-        msg_back = pickle.loads(rec)
-        print("Message-recived: {", msg_back.msg_sender, ",", msg_back.msg_reciver,
-          ",", msg_back.msg_hash, ",", msg_back.message, "}")
-        if msg_back.message == "OK":
-            message_not_send = False
-            
-    #message = msg.encode(FORMAT)
-    #msg_length = len(message)
-    #send_length = str(msg_length).encode(FORMAT)
-    #send_length += b' ' * (HEADER_SIZE - len(send_length))
-    #client.send(send_length)
-    #client.send(message)
+
+    global server_response_flag
+    server_response_flag = False
+
+    hashtmp = hashlib.sha256(msg.encode()).hexdigest()
+    m1 = Mess(nickname, receiver, hashtmp, msg)
+    m1Pickle = pickle.dumps(m1)
+    client.send(m1Pickle)
+
+    wait_iterator = 0
+
+    while not server_response_flag or wait_iterator > 10:
+        time.sleep(0.5)
+        wait_iterator += 1
+
+    if wait_iterator > 10:
+        print("server unreachable (send)")
+
+
+    # client.send(m1Pickle)
+    # rec = client.recv(4096)  # magic number
+    # msg_back = pickle.loads(rec)
+    # print("Message-received: {", msg_back.msg_sender, ",", msg_back.msg_receiver,
+    #      ",", msg_back.msg_hash, ",", msg_back.message, "}")
+    # if msg_back.message == "OK":
+    #    message_not_send = False
+
+    # message = msg.encode(FORMAT)
+    # msg_length = len(message)
+    # send_length = str(msg_length).encode(FORMAT)
+    # send_length += b' ' * (HEADER_SIZE - len(send_length))
+    # client.send(send_length)
+    # client.send(message)
+
 
 if __name__ == "__main__":
     print("[INFO:CLIENT] Starting client app...")
@@ -59,15 +119,20 @@ if __name__ == "__main__":
 
     print("Enter your nickname")
     nickname = input()
-    
+
+    thread = threading.Thread(target=client_receiver, args=())
+    thread.start()
+
     send_hello()
-    
+
     connected = True
     while connected:
-        user_input = input()
-        if user_input == "quit":
+        print("Enter receiver:")
+        user_input1 = input()
+        print("Enter message:")
+        user_input2 = input()
+        if user_input1 == "quit" or user_input2 == "quit":
             connected = False
-            send(DISCONNECTED_MESSAGE)
+            send("", DISCONNECTED_MESSAGE)
         else:
-            send(str(user_input))
-            
+            send(str(user_input1), str(user_input2))
--- messenger/server/server.py
+++ messenger/server/server.py
@@ -4,21 +4,22 @@ import hashlib
 import pickle
 
 class Mess:
-  def __init__(self, msg_sender, msg_reciver, msg_hash, message):
+  def __init__(self, msg_sender, msg_receiver, msg_hash, message):
     self.msg_sender = msg_sender
-    self.msg_reciver = msg_reciver
+    self.msg_receiver = msg_receiver
     self.msg_hash = msg_hash
     self.message = message
 
 #temporary databases
 
 clients = []
+nicknames = []
 
 #header defines how much bytes will be in the next message, and have size of 64 bytes. So maximum size of next message will be 2^64 bytes
 HEADER_SIZE = 64
 FORMAT ="utf-8"
 DISCONNECTED_MESSAGE = "###DISCONNECTED###"
-PORT = 5051
+PORT = 5050
 SERVER_IP = socket.gethostbyname(socket.gethostname()) ### this sometimes return loopback ip addres instead of just private network ip 127... instead of 196
 #to work this form internet, check for a public IP and put it here
 ADDR = (SERVER_IP, PORT)
@@ -34,32 +35,40 @@ server.bind(ADDR)
 
 def handle_client(conn, addr):
     print(f"[INFO] {addr} connected.")
+
     connected = True
     while connected:
         #msg_length = conn.recv(HEADER_SIZE).decode(FORMAT)
-        msg_length = conn.recv(4096)    #magic number
-        if msg_length:
-            msg = pickle.loads(msg_length)
+        msg_bits = conn.recv(4096)    #magic number
+        if msg_bits:
+            msg = pickle.loads(msg_bits)
             #msg_length = int(msg_length)
             #msg = conn.recv(msg_length).decode(FORMAT)
-            #if msg == DISCONNECTED_MESSAGE:
-            #    connected = False
+            if msg.message == DISCONNECTED_MESSAGE:
+                connected = False
             # action need to be taken from here. For now just print messages and address which they come from
-            print("[INFO] addr: {addr} msg: {msg.message}")
-            print("[INFO] addr: ", addr, "Message recived: {", msg.msg_sender, ",", msg.msg_reciver,
+            print("[INFO] addr: ", addr, "Message received: {", msg.msg_sender, ",", msg.msg_receiver,
                   ",", msg.msg_hash, ",", msg.message, "}")
 
             #message-cases
-            if msg.msg_reciver == "server-hello": #hello message
-                clients.append(msg.msg_sender)
+            if msg.msg_receiver == "server-hello": #hello message
+                clients.append(conn) #skleić client i nicknames w pary
+                nicknames.append(msg.msg_sender)
                 conn.send(pickle.dumps(Mess("server-hello-authentication", "", "", "OK")))
+            elif msg.msg_receiver == "server-hash-return" and msg.message == "OK":
+                #jest ok, doszla
+                #zrzuca z kolejki niewysłane wiadomości o hashu który teraz powrócił
+                print("message to ",msg.msg_sender, " was ok")
+            elif msg.msg_receiver == "server-hash-return" and msg.message == "NOK":
+                #nie jest ok, nie doszla
+                print("message to ", msg.msg_sender, " was Nok")
             else:   #normal message
                 localHash = hashlib.sha256(msg.message.encode()).hexdigest()
                 if localHash == msg.msg_hash:
-                    print("message is OK")
                     conn.send(pickle.dumps(Mess("server-authentication", "", "", "OK")))
-                    #add message to database
-                    #add message to sending_queue
+                    for i in range(len(nicknames)):
+                        if nicknames.__getitem__(i) == msg.msg_receiver:
+                            send_message(msg, msg_bits, clients.__getitem__(i))
                 else:
                     print("message is NOK")
                     conn.send(pickle.dumps(Mess("server-authentication", "", "", "NOK")))
@@ -67,6 +76,10 @@ def handle_client(conn, addr):
     conn.close()
 
 
+def send_message(msg,msg_bits,connection):
+    print("Sending message {", msg.msg_sender, ",", msg.msg_receiver,
+          ",", msg.msg_hash, ",", msg.message, "}")
+    connection.send(msg_bits)
 
 
 def start():
