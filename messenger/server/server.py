04c28bc251b37b0c21d99b6f1ca50a4bafc19ff2
Kacper <kacper.szatan@gmail.com>
2021-11-28 00:02:14 +0100

    server: refactor server and add basic functionality
    Wrapped functions of server in to the class. Added User class for
    managing clients. Refactor disconnecting action.

--- messenger/server/server.py
+++ messenger/server/server.py
@@ -2,97 +2,121 @@ import socket
 import threading
 import hashlib
 import pickle
-
-class Mess:
-  def __init__(self, msg_sender, msg_receiver, msg_hash, message):
-    self.msg_sender = msg_sender
-    self.msg_receiver = msg_receiver
-    self.msg_hash = msg_hash
-    self.message = message
-
-#temporary databases
-
-clients = []
-nicknames = []
-
-#header defines how much bytes will be in the next message, and have size of 64 bytes. So maximum size of next message will be 2^64 bytes
-HEADER_SIZE = 64
-FORMAT ="utf-8"
-DISCONNECTED_MESSAGE = "###DISCONNECTED###"
-PORT = 5050
-SERVER_IP = socket.gethostbyname(socket.gethostname()) ### this sometimes return loopback ip addres instead of just private network ip 127... instead of 196
-#to work this form internet, check for a public IP and put it here
-ADDR = (SERVER_IP, PORT)
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
+from messenger.common.constants import ClientRequestType as req_type
 
 
+class Server:
 
+    # active_IPs = []
+    active_users = list()
 
-#figure out method for detecting disconnected clients which has not log out properly by sending DISCONNECT_MESSAGE
+    def __init__(self, **kwargs):
+        # self.active_users = {} # user_id : ip_address : last seen time
+        self.header_size = 64
+        self.format = "utf-8"
+        # this sometimes return loopback ip addres instead of just private network ip. 127... instead of 196
+        # to work this from internet, check for a public IP and put it here
+        self.ip_address = socket.gethostbyname(socket.gethostname())
+        self.port = 6666
+
+        if "header_size" in kwargs:
+            self.header_size = kwargs["header_size"]
+        if "format" in kwargs:
+            self.format = kwargs["format"]
+        if "ip_address" in kwargs:
+            self.ip_address = kwargs["ip_address"]
+        if "port" in kwargs:
+            self.port = kwargs["port"]
+        if "header_size" in kwargs:
+            self.header_size = kwargs["header_size"]
+
+    def start(self):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((self.ip_address, self.port))
+        print("[INFO:SERVER] Starting server...")
+        server.listen()
+        print(f"[INFO:SERVER] Server is listening on {self.ip_address}")
+        while True:
+            conn, ip_addr = server.accept()
+            thread = threading.Thread(target=self.handle_client, args=(conn, ip_addr))
+            thread.start()
+            print(f"[INFO] ACTIVE CONNECTIONS: {threading.active_count() - 1}")
+
+    def get_clients_by_ip(self, ip_addr):
+        for user in self.active_users:
+            if user.ip_address == ip_addr:
+                return user
+        return None
+
+    def check_connection(self, ip_addr):
+        for user in self.active_users:
+            if user.ip_address == ip_addr:
+                if user.conn_status == "disconnected":
+                    return False
+                else:
+                    return True
+        return True
+
+    def remove_user(self, ip_addr):
+        for user in self.active_users:
+            if user.ip_address == ip_addr:
+                self.active_users.remove(user)
+
+    def handle_client(self, conn, ip_addr):
+        print(f"[INFO] {ip_addr} connected.")
+        connected = True
+        while connected:
+            msg_length = conn.recv(self.header_size).decode(self.format)
+            if msg_length:
+                msg_length = int(msg_length)
+                pickled_msg = conn.recv(msg_length)
+                self.handle_action(pickled_msg, ip_addr)
+            connected = self.check_connection(ip_addr)
+        self.remove_user(ip_addr)
+        conn.close()
+
+    def handle_action(self, pickled_client_request, ip_addr, conn):
+        client_request = pickle.loads(pickled_client_request)
+        # dev note: python 3.10 has switch statements... older versions doesn't
+        req = client_request.request_type
+        if req == req_type.LOG_IN:
+            self.action_login(client_request, ip_addr, conn)
+        elif req == req_type.DISCONNECT:
+            self.action_disconnect(ip_addr)
+        else:  # if clients talk non-existing requests -> disconnect them
+            self.action_disconnect(ip_addr)
+
+    def action_login(self, client_request, ip_addr, conn):
+        user = self.get_clients_by_ip(ip_addr)
+        if user is not None:
+            user.conn_status = "active"
+        else:
+            pass #.log_in()
+
+    def action_sync(self):
+        pass
+
+    def action_send_message(self):
+        pass
+
+    def action_disconnect(self, ip_addr):
+        for user in self.active_users:
+            if user.ip_address == ip_addr:
+                user.conn_status = "disconnected"  # potential locking needed for thread safe. Right now not needed
+                return
+        self.active_users.append(User(None, None, ip_addr, None, conn_status="disconnected"))
+
+# figure out method for detecting disconnected clients which has not log out properly by sending DISCONNECT_MESSAGE
 # probably ping  every 5 minutes all inactive user if they are online... If no response... remove them from active users list and kill their thread
 
-def handle_client(conn, addr):
-    print(f"[INFO] {addr} connected.")
 
-    connected = True
-    while connected:
-        #msg_length = conn.recv(HEADER_SIZE).decode(FORMAT)
-        msg_bits = conn.recv(4096)    #magic number
-        if msg_bits:
-            msg = pickle.loads(msg_bits)
-            #msg_length = int(msg_length)
-            #msg = conn.recv(msg_length).decode(FORMAT)
-            if msg.message == DISCONNECTED_MESSAGE:
-                connected = False
-            # action need to be taken from here. For now just print messages and address which they come from
-            print("[INFO] addr: ", addr, "Message received: {", msg.msg_sender, ",", msg.msg_receiver,
-                  ",", msg.msg_hash, ",", msg.message, "}")
-
-            #message-cases
-            if msg.msg_receiver == "server-hello": #hello message
-                clients.append(conn) #skleić client i nicknames w pary
-                nicknames.append(msg.msg_sender)
-                conn.send(pickle.dumps(Mess("server-hello-authentication", "", "", "OK")))
-            elif msg.msg_receiver == "server-hash-return" and msg.message == "OK":
-                #jest ok, doszla
-                #zrzuca z kolejki niewysłane wiadomości o hashu który teraz powrócił
-                print("message to ",msg.msg_sender, " was ok")
-            elif msg.msg_receiver == "server-hash-return" and msg.message == "NOK":
-                #nie jest ok, nie doszla
-                print("message to ", msg.msg_sender, " was Nok")
-            else:   #normal message
-                localHash = hashlib.sha256(msg.message.encode()).hexdigest()
-                if localHash == msg.msg_hash:
-                    conn.send(pickle.dumps(Mess("server-authentication", "", "", "OK")))
-                    for i in range(len(nicknames)):
-                        if nicknames.__getitem__(i) == msg.msg_receiver:
-                            send_message(msg, msg_bits, clients.__getitem__(i))
-                else:
-                    print("message is NOK")
-                    conn.send(pickle.dumps(Mess("server-authentication", "", "", "NOK")))
-
-    conn.close()
-
-
-def send_message(msg,msg_bits,connection):
-    print("Sending message {", msg.msg_sender, ",", msg.msg_receiver,
-          ",", msg.msg_hash, ",", msg.message, "}")
-    connection.send(msg_bits)
-
-
-def start():
-    server.listen()
-    print(f"[INFO:SERVER] Server is listening on {SERVER_IP}")
-    while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
-        thread.start()
-        print(f"[INFO] ACTIVE CONNECTIONS: {threading.active_count() - 1}")
-
-if __name__ == "__main__":
-    print("[INFO:SERVER] Starting server...")
-    print(socket.gethostname())
-    start()
-
+# move class to another module?
+class User:
+    def __init__(self, user_id, comm_socket, ip_address, last_seen, **kwargs):
+        self.user_id = user_id
+        self.communication_socket = comm_socket
+        self.ip_address = ip_address
+        self.last_seen = last_seen
+        self.conn_status = "active"  # active|disconnected
+        if "conn_status" in kwargs:
+            self.conn_status = kwargs["conn_status"]
